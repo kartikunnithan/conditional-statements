cdbf1306da61cbea804a5be176f9be5b6d87fd6f
kartikunnithan <kartikunnithan738@gmail.com>
2025-03-25 20:02:34 +0400

    4

---
+++ main4.py
@@ -0,0 +1,7 @@
+months=int(input("Enter a month:"))
+if months==January,february, march,april,may,june:
+    print("Its summer")
+elif months==july,august,september,october:
+    print("Its winter")
+else:
+    print("Its mild")
